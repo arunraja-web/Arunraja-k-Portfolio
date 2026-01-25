01147f407e09cd8aa1c434aefba20018832a2023
Arunraja K <rarun6706@gmail.com>
2026-01-25 21:10:42 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -57,7 +57,7 @@ const HeroSection = () => {
                 className="font-semibold"
                 asChild
               >
-                <a href="/ARUN RESUME.pdf" download>
+                <a href="/ARUN RESUME.pdf.pdf" download>
                   <Download className="mr-2 h-5 w-5" />
                   DOWNLOAD CV
                 </a>
